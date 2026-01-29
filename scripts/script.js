1ad0d29a3a8f61d7fe9f584ce84ad781d67331d6
StevenCesario <steven.lennartsson@gmail.com>
2026-01-29 10:25:21 +0100

    deleteListButton Event Listener implemented and super clean ✨ And the system works!

--- scripts/script.js
+++ scripts/script.js
@@ -114,7 +114,7 @@ contactULContainer.addEventListener('click', (e) => {
     // DETELE LOGIC
     if (e.target.classList.contains('delete-btn')) {
         const newContactList = DataStore.removeContact(id); // Update DataStore *first*
-        ViewRenderer.render(newContactList);                // Reflect in the ViewRenderer "Mirror"
+        ViewRenderer.render(newContactList);                // Reflect changes in the ViewRenderer "Mirror"
     }
 
     // EDIT/SAVE LOGIC
@@ -144,169 +144,180 @@ contactULContainer.addEventListener('click', (e) => {
     }
 })
 
-// C. Initiad Load
+// C. The "Remove List" Listener
+deleteListButton.addEventListener('click', (e) => {
+    e.preventDefault();
+
+    // Update DataStore with an empty list
+    DataStore.saveContacts([]);
+
+    // Reflect changes in the ViewRenderer "Mirror"
+    ViewRenderer.render([]);
+});
+
+// Initiad Load
 const initialContacts = DataStore.getContacts();
 ViewRenderer.render(initialContacts);
 
-let contactList;
-if (localStorage.getItem('contactData')) {
-    contactList = JSON.parse(localStorage.getItem('contactData'));
+// let contactList;
+// if (localStorage.getItem('contactData')) {
+//     contactList = JSON.parse(localStorage.getItem('contactData'));
 
-    for (let contactItem of contactList) {
-        let liElement = document.createElement('li');
-        // TODO: Går det att snygga upp dessa fyra rader i och med att vi upprepar oss med createElement?
-        let liElementName = document.createElement('input');
-        let liElementPhone = document.createElement('input');
-        let liElementEditButton = document.createElement('button');
-        let liElementRemoveButton = document.createElement('button');
+//     for (let contactItem of contactList) {
+//         let liElement = document.createElement('li');
+//         // TODO: Går det att snygga upp dessa fyra rader i och med att vi upprepar oss med createElement?
+//         let liElementName = document.createElement('input');
+//         let liElementPhone = document.createElement('input');
+//         let liElementEditButton = document.createElement('button');
+//         let liElementRemoveButton = document.createElement('button');
 
-        liElementEditButton.addEventListener('click', (e) => {
-            const newContactNameField = e.target.parentNode.children[0];
-            const newContactPhoneField = e.target.parentNode.children[1];
+//         liElementEditButton.addEventListener('click', (e) => {
+//             const newContactNameField = e.target.parentNode.children[0];
+//             const newContactPhoneField = e.target.parentNode.children[1];
 
-            if (newContactNameField.disabled == true) {
-                newContactNameField.disabled = false;
-                newContactPhoneField.disabled = false;
-            }
-            else {
-                if (newContactNameField.value.length === 0 || newContactPhoneField.value.length === 0) {
-                    errorMessageContainer.hidden = false;
-                    errorMessageContainer.textContent = errorMessageEdit;
-                    return
-                }
+//             if (newContactNameField.disabled == true) {
+//                 newContactNameField.disabled = false;
+//                 newContactPhoneField.disabled = false;
+//             }
+//             else {
+//                 if (newContactNameField.value.length === 0 || newContactPhoneField.value.length === 0) {
+//                     errorMessageContainer.hidden = false;
+//                     errorMessageContainer.textContent = errorMessageEdit;
+//                     return
+//                 }
 
-                errorMessageContainer.hidden = true;
-                newContactNameField.disabled = true;
-                newContactPhoneField.disabled = true;
+//                 errorMessageContainer.hidden = true;
+//                 newContactNameField.disabled = true;
+//                 newContactPhoneField.disabled = true;
 
-                for (let contactObject of contactList) {
-                    if (contactObject.id === contactId) {
-                        if (contactItem.contactName != newContactNameField.value) {
-                            newContactNameField.placeholder = newContactNameField.value;
-                        }
-                        if (contactItem.phone != newContactPhoneField.value) {
-                            newContactPhoneField.placeholder = newContactPhoneField.value;
-                        }
-                    }
-                }
+//                 for (let contactObject of contactList) {
+//                     if (contactObject.id === contactId) {
+//                         if (contactItem.contactName != newContactNameField.value) {
+//                             newContactNameField.placeholder = newContactNameField.value;
+//                         }
+//                         if (contactItem.phone != newContactPhoneField.value) {
+//                             newContactPhoneField.placeholder = newContactPhoneField.value;
+//                         }
+//                     }
+//                 }
 
-                localStorage.setItem('contactData', JSON.stringify(contactList));
-            }
+//                 localStorage.setItem('contactData', JSON.stringify(contactList));
+//             }
 
-            // TODO: Bug här all of a sudden?
-            e.target.textContent === "Ändra"
-                ? e.target.textContent = "Spara"
-                : e.target.textContent = "Ändra";
-        })
+//             // TODO: Bug här all of a sudden?
+//             e.target.textContent === "Ändra"
+//                 ? e.target.textContent = "Spara"
+//                 : e.target.textContent = "Ändra";
+//         })
 
-        liElementRemoveButton.addEventListener('click', (e) => {
-            deleteSingleContactListEntry(contactItem.id);
-            localStorage.setItem('contactData', JSON.stringify(contactList));
-            e.target.parentNode.remove(); 
-        })
+//         liElementRemoveButton.addEventListener('click', (e) => {
+//             deleteSingleContactListEntry(contactItem.id);
+//             localStorage.setItem('contactData', JSON.stringify(contactList));
+//             e.target.parentNode.remove(); 
+//         })
 
-        liElementName.placeholder = contactItem.contactName;
-        liElementPhone.placeholder = contactItem.phone;
-        liElementEditButton.textContent = "Ändra";
-        liElementRemoveButton.textContent = "Radera";
+//         liElementName.placeholder = contactItem.contactName;
+//         liElementPhone.placeholder = contactItem.phone;
+//         liElementEditButton.textContent = "Ändra";
+//         liElementRemoveButton.textContent = "Radera";
 
-        liElementName.disabled = true;
-        liElementPhone.disabled = true;
+//         liElementName.disabled = true;
+//         liElementPhone.disabled = true;
 
-        liElement.append(liElementName, liElementPhone, liElementEditButton, liElementRemoveButton);
-        contactULContainer.appendChild(liElement);
-    }
-}
-else {
-    contactList = [];
-}
+//         liElement.append(liElementName, liElementPhone, liElementEditButton, liElementRemoveButton);
+//         contactULContainer.appendChild(liElement);
+//     }
+// }
+// else {
+//     contactList = [];
+// }
 
-function deleteSingleContactListEntry(contactId) {
-    for (let [index, contactObject] of contactList.entries()) {
-        if (contactObject.id === contactId) {
-            contactList.splice(index, 1);
-        }
-    }
-}
+// function deleteSingleContactListEntry(contactId) {
+//     for (let [index, contactObject] of contactList.entries()) {
+//         if (contactObject.id === contactId) {
+//             contactList.splice(index, 1);
+//         }
+//     }
+// }
 
-createContactButton.addEventListener('click', (e) => {
-    e.preventDefault();
+// createContactButton.addEventListener('click', (e) => {
+//     e.preventDefault();
 
-    if (inputFormName.value.length === 0 || inputFormTel.value.length === 0) {
-        errorMessageContainer.hidden = false;
-        errorMessageContainer.textContent = errorMessageCreate;
-        return
-    }
+//     if (inputFormName.value.length === 0 || inputFormTel.value.length === 0) {
+//         errorMessageContainer.hidden = false;
+//         errorMessageContainer.textContent = errorMessageCreate;
+//         return
+//     }
 
-    // Om koden når hit har vi en valid entry. Sätt errorMessageContainer.hidden tillbaka till true
-    errorMessageContainer.hidden = true;
+//     // Om koden når hit har vi en valid entry. Sätt errorMessageContainer.hidden tillbaka till true
+//     errorMessageContainer.hidden = true;
     
-    let liElement = document.createElement('li');
-    let liElementName = document.createElement('input');
-    let liElementPhone = document.createElement('input');
-    let liElementEditButton = document.createElement('button');
-    let liElementRemoveButton = document.createElement('button');
+//     let liElement = document.createElement('li');
+//     let liElementName = document.createElement('input');
+//     let liElementPhone = document.createElement('input');
+//     let liElementEditButton = document.createElement('button');
+//     let liElementRemoveButton = document.createElement('button');
 
-    const contactID = crypto.randomUUID();
-    let listItemObject = {
-        id: contactID,
-        contactName: inputFormName.value,
-        phone: inputFormTel.value,
-    }
+//     const contactID = crypto.randomUUID();
+//     let listItemObject = {
+//         id: contactID,
+//         contactName: inputFormName.value,
+//         phone: inputFormTel.value,
+//     }
 
-    liElementName.placeholder = inputFormName.value;
-    liElementPhone.placeholder = inputFormTel.value;
-    liElementEditButton.textContent = "Ändra";
-    liElementRemoveButton.textContent = "Radera";
+//     liElementName.placeholder = inputFormName.value;
+//     liElementPhone.placeholder = inputFormTel.value;
+//     liElementEditButton.textContent = "Ändra";
+//     liElementRemoveButton.textContent = "Radera";
 
-    liElementName.disabled = true;
-    liElementPhone.disabled = true;
+//     liElementName.disabled = true;
+//     liElementPhone.disabled = true;
 
-    // liElementEditButton.addEventListener('click', (e) => {
-    //     if (e.target.disabled == true) {
-    //         e.target.disabled = false;
-    //     }
-    //     else {
-    //         e.target.disabled = true;
-    //     }
-    //     if (e.target.textContent === "Ändra") {
-    //         e.target.textContent = "Spara";
-    //     }
-    //     else {
-        //         e.target.textContent = "Ändra";
-        //     }
-        // })
+//     // liElementEditButton.addEventListener('click', (e) => {
+//     //     if (e.target.disabled == true) {
+//     //         e.target.disabled = false;
+//     //     }
+//     //     else {
+//     //         e.target.disabled = true;
+//     //     }
+//     //     if (e.target.textContent === "Ändra") {
+//     //         e.target.textContent = "Spara";
+//     //     }
+//     //     else {
+//         //         e.target.textContent = "Ändra";
+//         //     }
+//         // })
         
-    liElementRemoveButton.addEventListener('click', (e) => {
-        deleteSingleContactListEntry(contactID);
-        localStorage.setItem('contactData', JSON.stringify(contactList));
-        e.target.parentNode.remove(); 
-    })
+//     liElementRemoveButton.addEventListener('click', (e) => {
+//         deleteSingleContactListEntry(contactID);
+//         localStorage.setItem('contactData', JSON.stringify(contactList));
+//         e.target.parentNode.remove(); 
+//     })
     
-    // The first one is purely for CSS
-    liElement.append(
-        Object.assign(document.createElement('span'), { textContent: '•' }), 
-        liElementName, 
-        liElementPhone, 
-        liElementEditButton, 
-        liElementRemoveButton
-    );
+//     // The first one is purely for CSS
+//     liElement.append(
+//         Object.assign(document.createElement('span'), { textContent: '•' }), 
+//         liElementName, 
+//         liElementPhone, 
+//         liElementEditButton, 
+//         liElementRemoveButton
+//     );
     
-    contactULContainer.appendChild(liElement);
+//     contactULContainer.appendChild(liElement);
 
-    contactList.push(listItemObject);
-    localStorage.setItem('contactData', JSON.stringify(contactList));
+//     contactList.push(listItemObject);
+//     localStorage.setItem('contactData', JSON.stringify(contactList));
 
-    inputFormName.value = "";
-    inputFormTel.value = "";
-});
+//     inputFormName.value = "";
+//     inputFormTel.value = "";
+// });
 
-deleteListButton.addEventListener('click', (e) => {
-    e.preventDefault();
-    contactList.length = 0;
-    localStorage.setItem('contactData', JSON.stringify(contactList));
-    location.reload();
-})
+// deleteListButton.addEventListener('click', (e) => {
+//     e.preventDefault();
+//     contactList.length = 0;
+//     localStorage.setItem('contactData', JSON.stringify(contactList));
+//     location.reload();
+// })
 
 
 /**
